bcf9226b57e71c122cacdc0743b8d9a730551332
Thales Miguel Della Posta Montes <thalesiftm@gmail.com>
2023-02-20 09:57:43 -0300

    v1.0.4

--- git-website-front/src/components/pages/__App.js
+++ git-website-front/src/components/pages/__App.js
@@ -1,38 +1,29 @@
 import React from "react";
+import styles from "../../assets/styles/app.module.css";
+import { Link } from "react-router-dom";
 
 const App = ({ children }) => {
   return (
     <>
       <header>
-        <h1>My Website</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
+        <nav className={styles.menu}>
+          <Link to="/" className={styles.link}>
+            Ínicio
+          </Link>
         </nav>
       </header>
-      <main>{children}</main>
-      <footer>
-        <ul>
-          <li>
-            <a href="#">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="#">Terms of Use</a>
-          </li>
-          <li>
-            <a href="#">Contact Us</a>
-          </li>
-        </ul>
-        <p>&copy; 2023 My Website</p>
+      <main className={styles.main}>{children}</main>
+      <footer className={styles.footer}>
+        <div className={styles.links}>
+          <a
+            className={styles.link}
+            href="https://shawandpartners.com/full-back-front-test/"
+            target="blank"
+          >
+            Test conditions
+          </a>
+        </div>
+        <p className={styles.copyrights}>&copy; 2023 Web-app-git</p>
       </footer>
     </>
   );
